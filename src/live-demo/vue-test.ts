882e6e6ef985d5db51c078cf10f5007051164f9e
Nils Knappmeier <nils.knappmeier@cosee.biz>
2024-06-14 14:46:21 +0200

    test file for vue, just to get the link

---
+++ src/live-demo/vue-test.ts
@@ -0,0 +1,9 @@
+import {defineComponent} from "vue";
+
+// If you have checked this out, click on "defineComponent" to go the vue type-definitions and be amazed of how complex they are.
+defineComponent({
+    name: "vue-test",
+    props: {
+
+    }
+})
